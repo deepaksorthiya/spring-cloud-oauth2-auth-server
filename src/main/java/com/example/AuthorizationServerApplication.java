52e46abe245ecfa4d39d73dd6829276a13c96b0a
Deepak Katariya <deepak.sorthiya@gmail.com>
2019-09-15 10:58:16 +0530

    user info endpoint

--- src/main/java/com/example/AuthorizationServerApplication.java
+++ src/main/java/com/example/AuthorizationServerApplication.java
@@ -6,15 +6,9 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 
 @SpringBootApplication
 @EnableAuthorizationServer
-//@EnableResourceServer
-//@RestController
 public class AuthorizationServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AuthorizationServerApplication.class, args);
 	}
-	/*
-	 * @GetMapping("/userinfo") public Authentication getAuthentication() { return
-	 * SecurityContextHolder.getContext().getAuthentication(); }
-	 */
 }
