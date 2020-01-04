4d0d18821597a124507472f4d05c0575c2d4434f
Deepak Katariya <deepak.sorthiya@gmail.com>
2020-01-04 15:16:45 +0530

    h2 database config

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -23,16 +23,16 @@ DROP TABLE IF EXISTS oauth_approvals;
 DROP TABLE IF EXISTS ClientDetails;
 
 CREATE TABLE users(
-	username VARCHAR(255) NOT NULL PRIMARY KEY,
-	password VARCHAR(255) NOT NULL,
-	firstname VARCHAR(255),
-	lastname VARCHAR(255),
+	username VARCHAR_IGNORECASE NOT NULL PRIMARY KEY,
+	password VARCHAR_IGNORECASE  NOT NULL,
+	firstname VARCHAR_IGNORECASE,
+	lastname VARCHAR_IGNORECASE,
 	enabled boolean NOT NULL
 );
 
 CREATE TABLE authorities (
-	username VARCHAR(255) NOT NULL,
-	authority VARCHAR(255) NOT NULL,
+	username VARCHAR_IGNORECASE NOT NULL,
+	authority VARCHAR_IGNORECASE NOT NULL,
 	CONSTRAINT FK_authorities_users FOREIGN KEY(username) REFERENCES users(username)
 );
 
@@ -40,81 +40,81 @@ CREATE UNIQUE INDEX UQ_authorities_username_authority ON authorities (username,a
 
 CREATE TABLE groups (
   id BIGINT PRIMARY KEY,
-  group_name VARCHAR(255) NOT NULL);
+  group_name VARCHAR_IGNORECASE NOT NULL);
 
 CREATE TABLE group_authorities (
   group_id BIGINT NOT NULL,
-  authority VARCHAR(255) NOT NULL,
+  authority VARCHAR_IGNORECASE NOT NULL,
   CONSTRAINT FK_group_authorities_group FOREIGN KEY(group_id) REFERENCES groups(id));
 
 CREATE TABLE group_members (
   id BIGINT PRIMARY KEY,
-  username VARCHAR(255) NOT NULL,
+  username VARCHAR_IGNORECASE NOT NULL,
   group_id bigint NOT NULL,
   CONSTRAINT FK_group_members_group FOREIGN KEY(group_id) REFERENCES groups(id));
   
 
 CREATE TABLE oauth_client_details (
-  client_id VARCHAR(255) PRIMARY KEY,
-  resource_ids VARCHAR(255),
-  client_secret LONG VARBINARY,
-  scope LONG VARBINARY,
-  authorized_grant_types VARCHAR(255),
-  web_server_redirect_uri LONG VARBINARY,
-  authorities VARCHAR(255),
+  client_id VARCHAR_IGNORECASE PRIMARY KEY,
+  resource_ids VARCHAR_IGNORECASE,
+  client_secret VARCHAR_IGNORECASE,
+  scope VARCHAR_IGNORECASE,
+  authorized_grant_types VARCHAR_IGNORECASE,
+  web_server_redirect_uri VARCHAR_IGNORECASE,
+  authorities VARCHAR_IGNORECASE,
   access_token_validity INTEGER,
   refresh_token_validity INTEGER,
   additional_information VARCHAR(4096),
-  autoapprove VARCHAR(255)
+  autoapprove VARCHAR_IGNORECASE
 );
 
 CREATE TABLE oauth_client_token (
-  token_id VARCHAR(255),
-  token LONG VARBINARY,
-  authentication_id VARCHAR(255) PRIMARY KEY,
-  user_name VARCHAR(255),
-  client_id VARCHAR(255)
+  token_id VARCHAR_IGNORECASE,
+  token VARCHAR_IGNORECASE,
+  authentication_id VARCHAR_IGNORECASE PRIMARY KEY,
+  user_name VARCHAR_IGNORECASE,
+  client_id VARCHAR_IGNORECASE
 );
 
 CREATE TABLE oauth_access_token (
-  token_id VARCHAR(255),
-  token LONG VARBINARY,
-  authentication_id VARCHAR(255) PRIMARY KEY,
-  user_name VARCHAR(255),
-  client_id VARCHAR(255),
-  authentication LONG VARBINARY,
-  refresh_token VARCHAR(255)
+  token_id VARCHAR_IGNORECASE,
+  token VARCHAR_IGNORECASE,
+  authentication_id VARCHAR_IGNORECASE PRIMARY KEY,
+  user_name VARCHAR_IGNORECASE,
+  client_id VARCHAR_IGNORECASE,
+  authentication VARCHAR_IGNORECASE,
+  refresh_token VARCHAR_IGNORECASE
 );
 
 CREATE TABLE oauth_refresh_token (
-  token_id VARCHAR(255),
-  token LONG VARBINARY,
-  authentication LONG VARBINARY
+  token_id VARCHAR_IGNORECASE,
+  token VARCHAR_IGNORECASE,
+  authentication VARCHAR_IGNORECASE
 );
 
 CREATE TABLE oauth_code (
-  code VARCHAR(255), authentication LONG VARBINARY
+  code VARCHAR_IGNORECASE, authentication VARCHAR_IGNORECASE
 );
 
 CREATE TABLE oauth_approvals (
-	userId VARCHAR(255),
-	clientId VARCHAR(255),
-	scope VARCHAR(255),
+	userId VARCHAR_IGNORECASE,
+	clientId VARCHAR_IGNORECASE,
+	scope VARCHAR_IGNORECASE,
 	status VARCHAR(10),
 	expiresAt TIMESTAMP,
 	lastModifiedAt TIMESTAMP
 );
 
 CREATE TABLE ClientDetails (
-  appId VARCHAR(255) PRIMARY KEY,
-  resourceIds VARCHAR(255),
-  appSecret VARCHAR(255),
-  scope VARCHAR(255),
-  grantTypes VARCHAR(255),
-  redirectUrl VARCHAR(255),
-  authorities VARCHAR(255),
+  appId VARCHAR_IGNORECASE PRIMARY KEY,
+  resourceIds VARCHAR_IGNORECASE,
+  appSecret VARCHAR_IGNORECASE,
+  scope VARCHAR_IGNORECASE,
+  grantTypes VARCHAR_IGNORECASE,
+  redirectUrl VARCHAR_IGNORECASE,
+  authorities VARCHAR_IGNORECASE,
   access_token_validity INTEGER,
   refresh_token_validity INTEGER,
   additionalInformation VARCHAR(4096),
-  autoApproveScopes VARCHAR(255)
+  autoApproveScopes VARCHAR_IGNORECASE
 );
